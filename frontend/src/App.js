15be37761a77709f4477acc311a844219b6f3f4a
Yasiru Nisal <135297175+Yasirunk9612@users.noreply.github.com>
2025-05-16 01:30:05 +0530

    add ne code app, app.test and  yml

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
+// src/App.js
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CheckoutForm from "./compo/pages/CheckoutForm";
 import Payments from "./compo/pages/Payment";
 import Summary from "./compo/pages/Summary";
 
-
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/checkout-form" element={<CheckoutForm />} />
-        <Route path="/payment" element={<Payments/>} />
-        <Route path="/summary" element={<Summary/>} /> 
-      </Routes>
+      <div>
+        <h1>Welcome to my shop</h1>
+        <Routes>
+          <Route path="/checkout-form" element={<CheckoutForm />} />
+          <Route path="/payment" element={<Payments />} />
+          <Route path="/summary" element={<Summary />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
